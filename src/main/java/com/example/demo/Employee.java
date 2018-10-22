2c8e6b57563d43381a09060487f88d7b151c25bb
morganm22 <millermorgan98@gmail.com>
2018-10-22 17:37:12 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+public class Employee {
+    private long id;
+    private String Name;
+    private long SSN;
+    private String DOB;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public long getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(long SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+}
